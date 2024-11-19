9d87c281110c3715b2d8b4ef9caa381de02fe02b
ufoo68 <mitarou.yuutarou@gmail.com>
2024-11-19 19:22:37 +0900

    parser途中まで

---
+++ saba_core/src/renderer/dom/node.rs
@@ -0,0 +1,165 @@
+use alloc::rc::Rc;
+use alloc::rc::Weak;
+use core::cell::RefCell;
+use core::str::FromStr;
+use alloc::string::String;
+use crate::renderer::html::attribute::Attribute;
+use alloc::vec::Vec;
+use alloc::format;
+
+#[derive(Debug, Clone)]
+pub struct Node {
+  pub kind: NodeKind,
+  window: Weak<RefCell<Window>>,
+  parent: Weak<RefCell<Node>>,
+  first_child: Option<Rc<RefCell<Node>>>,
+  last_child: Weak<RefCell<Node>>,
+  previous_sibling: Weak<RefCell<Node>>,
+  next_sibling: Option<Rc<RefCell<Node>>>,
+}
+
+impl Node {
+  pub fn new(kind: NodeKind) -> Rc<RefCell<Node>> {
+    Rc::new(RefCell::new(Node {
+      kind,
+      window: Weak::new(),
+      parent: Weak::new(),
+      first_child: None,
+      last_child: Weak::new(),
+      previous_sibling: Weak::new(),
+      next_sibling: None,
+    }))
+  }
+
+  pub fn set_parent(&mut self, parent: Weak<RefCell<Node>>) {
+    self.parent = parent;
+  }
+
+  pub fn parent(&self) -> Weak<RefCell<Node>> {
+    self.parent.clone()
+  }
+
+  pub fn set_first_child(&mut self, first_child: Option<Rc<RefCell<Node>>>) {
+    self.first_child = first_child;
+  }
+
+  pub fn first_child(&self) -> Option<Rc<RefCell<Node>>> {
+    self.first_child.as_ref().cloned()
+  }
+
+  pub fn set_last_child(&mut self, last_child: Weak<RefCell<Node>>) {
+    self.last_child = last_child;
+  }
+
+  pub fn last_child(&self) -> Weak<RefCell<Node>> {
+    self.last_child.clone()
+  }
+
+  pub fn set_previous_sibling(&mut self, previous_sibling: Weak<RefCell<Node>>) {
+    self.previous_sibling = previous_sibling;
+  }
+
+  pub fn previous_sibling(&self) -> Weak<RefCell<Node>> {
+    self.previous_sibling.clone()
+  }
+
+  pub fn set_next_sibling(&mut self, next_sibling: Option<Rc<RefCell<Node>>>) {
+    self.next_sibling = next_sibling;
+  }
+
+  pub fn next_sibling(&self) -> Option<Rc<RefCell<Node>>> {
+    self.next_sibling.as_ref().cloned()
+  }
+
+  pub fn set_window(&mut self, window: Weak<RefCell<Window>>) {
+    self.window = window;
+  }
+
+  pub fn kind (&self) -> NodeKind {
+    self.kind.clone()
+  }
+
+  pub fn get_element(&self) -> Option<Element> {
+    match self.kind {
+      NodeKind::Document | NodeKind::Text(_) => None,
+      NodeKind::Element(ref element) => Some(element.clone()),
+    }
+  }
+
+  pub fn element_kind(&self) -> Option<ElementKind> {
+    match self.kind {
+      NodeKind::Document | NodeKind::Text(_) => None,
+      NodeKind::Element(ref element) => Some(element.kind()),
+    }
+  }
+}
+
+#[derive(Debug, Clone)]
+pub enum NodeKind {
+  Document,
+  Element(Element),
+  Text(String),
+}
+
+#[derive(Debug, Clone)]
+pub struct Window {
+  document: Rc<RefCell<Node>>,
+}
+
+impl Window {
+  pub fn new() -> Self {
+    let window = Self {
+      document: Node::new(NodeKind::Document),
+    };
+
+    window.document.borrow_mut().set_window(Rc::downgrade(&Rc::new(RefCell::new(window.clone()))));
+    window
+  }
+
+  pub fn document(&self) -> Rc<RefCell<Node>> {
+    self.document.clone()
+  }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Element {
+  kind: ElementKind,
+  attributes: Vec<Attribute>,
+}
+
+impl Element {
+  pub fn new(element_name: &str, attributes: Vec<Attribute>) -> Self {
+    Self {
+      kind: ElementKind::from_str(element_name).expect("failed to parse ElementKind"),
+      attributes: Vec::new(),
+    }
+  }
+
+  pub fn kind(&self) -> ElementKind {
+    self.kind.clone()
+  }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum ElementKind {
+  Html,
+  Head,
+  Style,
+  Script,
+  Body,
+}
+
+impl FromStr for ElementKind {
+  type Err = String;
+
+  fn from_str(s: &str) -> Result<Self, Self::Err> {
+    match s {
+      "html" => Ok(ElementKind::Html),
+      "head" => Ok(ElementKind::Head),
+      "style" => Ok(ElementKind::Style),
+      "script" => Ok(ElementKind::Script),
+      "body" => Ok(ElementKind::Body),
+      _ => Err(format!("failed to parse ElementKind: {:?}", s)),
+    }
+  }
+}
