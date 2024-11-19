2910c02dd2864f7cf39187e1d12c0abddab4f3cc
ufoo68 <mitarou.yuutarou@gmail.com>
2024-11-19 18:43:10 +0900

    tokenizer実装完了まで

--- saba_core/src/renderer/html/token.rs
+++ saba_core/src/renderer/html/token.rs
@@ -112,6 +112,21 @@ impl HtmlTokenizer {
       }
     }
   }
+
+  fn set_self_closing_flag(&mut self) {
+    assert!(self.latest_token.is_some());
+
+    if let Some(t) = self.latest_token.as_mut() {
+      match t {
+        HtmlToken::StartTag {
+          tag: _,
+          ref mut self_closing,
+          attributes: _,
+        } => *self_closing = true,
+        _ => panic!("unexpected token"),
+      }
+    }
+  }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -313,8 +328,273 @@ impl Iterator for HtmlTokenizer {
           self.reconsume = true;
           self.state = State::AttributeValueUnquoted;
         }
-        _ => {}
+
+        State::AttributeValueDoubleQuoted => {
+          if c == '"' {
+            self.state = State::AfterAttributeValueQuoted;
+            continue;
+          }
+
+          if self.is_eof() {
+            return Some(HtmlToken::Eof);
+          }
+
+          self.append_attribute(c, false);
+        }
+
+        State::AttributeValueSingleQuoted => {
+          if c == '\'' {
+            self.state = State::AfterAttributeValueQuoted;
+            continue;
+          }
+
+          if self.is_eof() {
+            return Some(HtmlToken::Eof);
+          }
+
+          self.append_attribute(c, false);
+        }
+
+        State::AttributeValueUnquoted => {
+          if c == ' ' {
+            self.state = State::BeforeAttributeName;
+            continue;
+          }
+
+          if c == '>' {
+            self.state = State::Data;
+            return self.take_latest_token();
+          }
+
+          if self.is_eof() {
+            return Some(HtmlToken::Eof);
+          }
+
+          self.append_attribute(c, false);
+        }
+
+        State::AfterAttributeValueQuoted => {
+          if c == ' ' {
+            self.state = State::BeforeAttributeName;
+            continue;
+          }
+
+          if c == '/' {
+            self.state = State::SelfClosingStartTag;
+            continue;
+          }
+
+          if c == '>' {
+            self.state = State::Data;
+            return self.take_latest_token();
+          }
+
+          if self.is_eof() {
+            return Some(HtmlToken::Eof);
+          }
+
+          self.reconsume = true;
+          self.state = State::BeforeAttributeName;
+        }
+
+        State::SelfClosingStartTag => {
+          if c == '>' {
+            self.set_self_closing_flag();
+            self.state = State::Data;
+            return self.take_latest_token();
+          }
+
+          if self.is_eof() {
+            return Some(HtmlToken::Eof);
+          }
+
+          self.reconsume = true;
+          self.state = State::BeforeAttributeName;
+        }
+
+        State::ScriptData => {
+          if c == '<' {
+            self.state = State::ScriptDataLessThanSign;
+            continue;
+          }
+
+          if self.is_eof() {
+            return Some(HtmlToken::Eof);
+          }
+
+          return Some(HtmlToken::Char(c));
+        }
+
+        State::ScriptDataLessThanSign => {
+          if c == '/' {
+            self.buf = String::new();
+            self.state = State::ScriptDataEndTagOpen;
+            continue;
+          }
+
+          self.reconsume = true;
+          self.state = State::ScriptData;
+          return Some(HtmlToken::Char('<'));
+        }
+
+        State::ScriptDataEndTagOpen => {
+          if c.is_ascii_alphabetic() {
+            self.reconsume = true;
+            self.state = State::ScriptDataEndTagName;
+            self.create_tag(false);
+            continue;
+          }
+
+          self.reconsume = true;
+          self.state = State::ScriptData;
+          return Some(HtmlToken::Char('<'));
+        }
+
+        State::ScriptDataEndTagName => {
+          if c == '>' {
+            self.state = State::Data;
+            return self.take_latest_token();
+          }
+
+          if c.is_ascii_alphabetic() {
+            self.buf.push(c);
+            self.append_tag_name(c.to_ascii_lowercase());
+            continue;
+          }
+
+          self.state = State::TemporaryBuffer;
+          self.buf = String::from("</") + &self.buf;
+          self.buf.push(c);
+          continue;
+        }
+        
+        State::TemporaryBuffer => {
+          self.reconsume = true;
+
+          if self.buf.chars().count() == 0 {
+            self.state = State::ScriptData;
+            continue;
+          }
+
+          let c = self.buf.chars().nth(0).expect("self.buf should have at least one character");
+          self.buf.remove(0);
+          return Some(HtmlToken::Char(c));
+        }
       }
     }
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use crate::alloc::string::ToString;
+  use alloc::vec;
+
+  #[test]
+  fn test_empty() {
+    let html = "".to_string();
+    let mut tokenizer = HtmlTokenizer::new(html);
+    assert_eq!(tokenizer.next(), None);
+  }
+
+  #[test]
+  fn test_start_and_end_tag() {
+    let html = "<div></div>".to_string();
+    let mut tokenizer = HtmlTokenizer::new(html);
+    assert_eq!(tokenizer.next(), Some(HtmlToken::StartTag {
+      tag: "div".to_string(),
+      self_closing: false,
+      attributes: Vec::new(),
+    }));
+    assert_eq!(tokenizer.next(), Some(HtmlToken::EndTag {
+      tag: "div".to_string(),
+    }));
+    assert_eq!(tokenizer.next(), None);
+  }
+
+  #[test]
+  fn test_attributes() {
+    let html = "<div class=\"foo\" id='bar'></div>".to_string();
+    let mut tokenizer = HtmlTokenizer::new(html);
+    let mut attr1 = Attribute::new();
+    attr1.add_char('c', true);
+    attr1.add_char('l', true);
+    attr1.add_char('a', true);
+    attr1.add_char('s', true);
+    attr1.add_char('s', true);
+    attr1.add_char('f', false);
+    attr1.add_char('o', false);
+    attr1.add_char('o', false);
+    let mut attr2 = Attribute::new();
+    attr2.add_char('i', true);
+    attr2.add_char('d', true);
+    attr2.add_char('b', false);
+    attr2.add_char('a', false);
+    attr2.add_char('r', false);
+
+    assert_eq!(tokenizer.next(), Some(HtmlToken::StartTag {
+      tag: "div".to_string(),
+      self_closing: false,
+      attributes: vec![
+        attr1,
+        attr2,
+      ],
+    }));
+    assert_eq!(tokenizer.next(), Some(HtmlToken::EndTag {
+      tag: "div".to_string(),
+    }));
+    assert_eq!(tokenizer.next(), None);
+  }
+
+  #[test]
+  fn test_self_closing_tag() {
+    let html = "<img />".to_string();
+    let mut tokenizer = HtmlTokenizer::new(html);
+    assert_eq!(tokenizer.next(), Some(HtmlToken::StartTag {
+      tag: "img".to_string(),
+      self_closing: true,
+      attributes: Vec::new(),
+    }));
+    assert_eq!(tokenizer.next(), None);
+  }
+
+  #[test]
+  fn test_script_tag() {
+    let html = "<script>js</script>".to_string();
+    let mut tokenizer = HtmlTokenizer::new(html);
+    assert_eq!(tokenizer.next(), Some(HtmlToken::StartTag {
+      tag: "script".to_string(),
+      self_closing: false,
+      attributes: Vec::new(),
+    }));
+    assert_eq!(tokenizer.next(), Some(HtmlToken::Char('j')));
+    assert_eq!(tokenizer.next(), Some(HtmlToken::Char('s')));
+    assert_eq!(tokenizer.next(), Some(HtmlToken::EndTag {
+      tag: "script".to_string(),
+    }));
+    assert_eq!(tokenizer.next(), None);
+  }
+
+  #[test]
+  fn test_div_content() {
+    let html = "<div>content</div>".to_string();
+    let mut tokenizer = HtmlTokenizer::new(html);
+    assert_eq!(tokenizer.next(), Some(HtmlToken::StartTag {
+      tag: "div".to_string(),
+      self_closing: false,
+      attributes: Vec::new(),
+    }));
+    assert_eq!(tokenizer.next(), Some(HtmlToken::Char('c')));
+    assert_eq!(tokenizer.next(), Some(HtmlToken::Char('o')));
+    assert_eq!(tokenizer.next(), Some(HtmlToken::Char('n')));
+    assert_eq!(tokenizer.next(), Some(HtmlToken::Char('t')));
+    assert_eq!(tokenizer.next(), Some(HtmlToken::Char('e')));
+    assert_eq!(tokenizer.next(), Some(HtmlToken::Char('n')));
+    assert_eq!(tokenizer.next(), Some(HtmlToken::Char('t')));
+    assert_eq!(tokenizer.next(), Some(HtmlToken::EndTag {
+      tag: "div".to_string(),
+    }));
+    assert_eq!(tokenizer.next(), None);
   }
 }
